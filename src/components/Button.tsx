db0880c117dcf2ad09eb5aee655c1ee50459bc70
Fumiya Takahashi <fumirou0525@gmail.com>
2023-03-03 23:24:09 +0900

    webpackの修正となぜかステージ出来ていなかったファイルを追加

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Choice } from "./ButtonGroup";
+
+interface ButtonProps {
+    choice: Choice;
+    onSelected: (isAnswer: boolean) => void;
+}
+
+const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
+    const { onSelected, choice } = props;
+    const onClick = () => {
+        onSelected(choice.isAnswer);
+    }
+
+    return (
+        <>
+            <input type="button" value={choice.value} className="w-full bg-indigo-400 hover:bg-indigo-300 font-bold  text-white px-4 py-2 rounded-full" onClick={onClick} />
+        </>
+    )
+}
+
+export default Button;
