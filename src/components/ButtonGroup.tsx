23570efee00f12bd8c39f484d6fa72c7e6d87767
fumirou05 <fumirou0525@gmail.com>
2023-03-03 23:27:57 +0900

    コンポーネント修正webpack修正 (#13)

---
+++ src/components/ButtonGroup.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Button from "./Button";
+
+export interface Choice {
+    value: string,
+    isAnswer: boolean
+}
+
+interface ButtonGroupProps {
+    choices: Choice[]
+    incrementQNum: () => void;
+    incrementCorrect: () => void;
+}
+
+const ButtonGroup: React.FC<ButtonGroupProps> = (props: ButtonGroupProps) => {
+    const { choices, incrementQNum, incrementCorrect } = props;
+
+    const onSelected = (isAnswer: boolean) => {
+        if (isAnswer) {
+            alert("correct!");
+            incrementCorrect();
+        } else {
+            alert("incorrect");
+        }
+        incrementQNum();
+    }
+
+    return (
+        <>
+            <div className="flex-col">
+                {choices.map((c, i) => {
+                    return (
+                        <div className="my-2">
+                            <Button key={i} choice={c} onSelected={onSelected} />
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+export default ButtonGroup;
